3ba2f8221992637b42e7fcf2c0a64801a7121510
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-07-12 18:44:42 -0600

    added database sql pizzapalacedb

---
+++ database_sql/pizzapalacedb.sql
@@ -0,0 +1,131 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: pizzapalacedb
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `complement`
+--
+
+DROP TABLE IF EXISTS `complement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `complement` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) NOT NULL,
+  `code` varchar(45) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `complement`
+--
+
+LOCK TABLES `complement` WRITE;
+/*!40000 ALTER TABLE `complement` DISABLE KEYS */;
+INSERT INTO `complement` VALUES (1,'Deditos de queso','deditos',4.00),(2,'Pan con Ajo','panajo',5.00),(3,'Nuditos','nuditos',2.00);
+/*!40000 ALTER TABLE `complement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `extra`
+--
+
+DROP TABLE IF EXISTS `extra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `extra` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) NOT NULL,
+  `code` varchar(45) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `extra`
+--
+
+LOCK TABLES `extra` WRITE;
+/*!40000 ALTER TABLE `extra` DISABLE KEYS */;
+INSERT INTO `extra` VALUES (1,'Postre','postre',4.00),(2,'Wings','wings',5.00),(3,'Bebida','bebida',2.00);
+/*!40000 ALTER TABLE `extra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `flavor`
+--
+
+DROP TABLE IF EXISTS `flavor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flavor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) NOT NULL,
+  `code` varchar(45) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flavor`
+--
+
+LOCK TABLES `flavor` WRITE;
+/*!40000 ALTER TABLE `flavor` DISABLE KEYS */;
+INSERT INTO `flavor` VALUES (1,'Pepperoni','pepperoni',1.00),(2,'Hawaiana','hawaiana',2.00),(3,'Meatlovers','meatlovers',3.00),(4,'Mixed','mixed',5.00);
+/*!40000 ALTER TABLE `flavor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `size`
+--
+
+DROP TABLE IF EXISTS `size`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `size` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(55) NOT NULL,
+  `value` int NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `size`
+--
+
+LOCK TABLES `size` WRITE;
+/*!40000 ALTER TABLE `size` DISABLE KEYS */;
+INSERT INTO `size` VALUES (1,'1 pizza',1,11.00),(2,'2 pizzas',2,17.00),(3,'3 pizzas',3,24.00),(5,'4 pizzas',4,40.00);
+/*!40000 ALTER TABLE `size` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-12 18:43:35
